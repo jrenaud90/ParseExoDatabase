f5348edbe1da1800a51a0b209f328260b3ae7c1e
Joe Renaud <jrenaud@masonlive.gmu.edu>
2014-05-21 00:43:54 -0400

    V0_6
    Have a link out file. Now to test it with actual html in question.

--- Parse.py
+++ Parse.py
@@ -8,47 +8,6 @@ def deleteContent(pfile): #Delete File Contents
     pfile.seek(0)
     pfile.truncate()
     return pfile
-
-#Global Vars
-now = time.strftime("%c")
-excFilePath = inspect.getfile(inspect.currentframe()) #Finds current file path including file name.
-
-#Open and use log file
-path = excFilePath.replace("Parse.py", "") + 'Output.log' #Gets rid of current file name and replaces it with log name, change parse.py to current file name.
-filetmp = open(path,'w')
-log = deleteContent(filetmp)
-LogStartStr = 'Exoplanet HTML Database Parse Ver 0\nScript by Joe Renaud\nLog file created on: ' + now + '\n\n' + '1-Begin...\n'
-log.write(LogStartStr)
-filetmp = None
-path = None
-
-#Open HTML in question
-HtmlURL = 'http://tedxgeorgemasonu.com/thankyou'
-HtmlData = urllib2.urlopen(HtmlURL)
-path = excFilePath.replace("Parse.py", "") + 'html1.tmp'
-log.write('2-HTML URL to open: ' + HtmlURL + '\n')
-filetmp = open(path,'w')
-html1 = deleteContent(filetmp)
-for line in HtmlData:
-    html1.write(line)
-log.write('3-Wrote HTML to : ' + path + '\n')
-html1 = None
-html1 = open(path,'r')
-log.write('4-Reading ' + path + 'as var: html1')
-path = None
-filetmp = None
-kstr = ''
-for line in html1:
-    kstr = kstr + line #Make large string of the html file
-html1.close
-
-#Make temp files for storing html information
-path = excFilePath.replace("Parse.py", "") + 'htmldata.tmp'
-filetmp = open(path,'w')
-htmldata = deleteContent(filetmp)
-path = None
-filetmp = None
-
 #create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
     def handle_data(self, data):
@@ -72,39 +31,79 @@ class MyHTMLParser(HTMLParser):
 #        print "Encountered an end tag :", tag
 #    def handle_data(self, data):
 #        print "Encountered some data  :", data
+        
+#Global Vars
+now = time.strftime("%c")
+excFilePath = inspect.getfile(inspect.currentframe()) #Finds current file path including file name.
 
+#Open and use log file
+path = excFilePath.replace("Parse.py", "") + 'Output.log' #Gets rid of current file name and replaces it with log name, change parse.py to current file name.
+filetmp = open(path,'w')
+log = deleteContent(filetmp)
+LogStartStr = 'Exoplanet HTML Database Parse Ver 0\nScript by Joe Renaud\nLog file created on: ' + now + '\n\n' + '1-Begin...\n'
+log.write(LogStartStr)
+filetmp = None
+path = None
+
+#Open HTML in question
+HtmlURL = 'http://tedxgeorgemasonu.com/thankyou'
+HtmlData = urllib2.urlopen(HtmlURL)
+##
+path = excFilePath.replace("Parse.py", "") + 'html1.tmp'
+log.write('2-HTML URL to open: ' + HtmlURL + '\n')
+with open(path,'w') as filetmp:
+    with deleteContent(filetmp) as html1:
+        for line in HtmlData:
+            html1.write(line)
+        log.write('3-Wrote HTML to : ' + path + '\n')
+        pass
+    pass
+with open(path,'r') as html1:
+    log.write('4-Reading ' + path + 'as var: html1')
+    kstr = ''
+    for line in html1:
+        kstr = kstr + line #Make large string of the html file
+    pass
+
+#Make temp files for storing html information
+path = excFilePath.replace("Parse.py", "") + 'htmldata.tmp'
+with open(path,'w') as filetmp:
+    with deleteContent(filetmp) as htmldata:
 # instantiate the parser and fed it some HTML
-parser = MyHTMLParser()
-parser.feed(kstr)
-try:
-    log.write('5-Finished parsing the html document')
-    htmldata.close
-    path = excFilePath.replace("Parse.py", "") + 'htmldata.tmp'
-    htmldata = open(path,'r')
-    log.write('6-Opened htmldata.tmp in readonly mode')
-    htmldatasmall = mmap.mmap(htmldata.fileno(),0, access=mmap.ACCESS_READ)
-    log.write('7-Moved htmldata.tmp into smaller access space using mmap')
-    path = None
+        parser = MyHTMLParser()
+        parser.feed(kstr)
+        pass
+    pass
+log.write('5-Finished parsing the html document\n')
+path = excFilePath.replace("Parse.py", "") + 'htmldata.tmp'
+with open(path,'r') as htmldata:
+    log.write('6-Opened htmldata.tmp in readonly mode\n')
 
 # Find links in the file
+    log.write('7-Looking for links...\n')
     path = excFilePath.replace("Parse.py", "") + 'links.tmp'
-    filetmp = open(path,'w')
-    linkfile = deleteContent(filetmp)
-    path = None
-    filetmp = None
-
-    if htmldatasmall.find('::TAG: a') > -1:
-        x = htmldata.readlines()
-        for i in x:
-            print x[i]
+    with open(path,'w') as filetmp:
+        with deleteContent(filetmp) as linkfile:
+            x = htmldata.readlines()
+            a = 0
+            for i in x:
+                if a == 1:
+                    log.write('\t Link Found!\n')
+                    indx = i.index('=') +2
+                    link = i[indx:]
+                    linkfile.write(link)
+                    log.write('\t Link Recorded!\n')
+                if i.find('::TAG: a') == 0:
+                    a = 1
+                else:
+                    a = 0
         #if '::TAG: a' in line:
             #print htmldata[]
-        else:
-            linkfile.write('NO LINKS FOUND')
-            log.write('9-No links were found in html data')
-finally:
-    htmldatasmall.close
-    htmldata.close
+            pass
+        pass
+    pass
+
+
 
 #Debuging
 #os.remove(excFilePath.replace("Parse.py", "") + 'html1.tmp') #Uncomment during debug.
